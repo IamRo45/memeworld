c053cb76813cb559cda0273b2012f1034c609390
IamRo45 <rohit01051999@gmail.com>
2021-02-25 14:40:43 +0530

    install.sh modified

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
-sudo apt-get install -y python3.7
-sudo apt-get install -y python3-pip
 
-pip3 install flask
-pip3 install flask-sqlalchemy
-pip3 install flask-migrate
-pip3 install -U flask-cors
+sudo apt-get install -y python-pip
+
+pip install flask
+pip install flask-sqlalchemy
+pip install flask-migrate
+pip install -U flask-cors
 
